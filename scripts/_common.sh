7f498b1d4ea27c55d8ebf2e71f1e93e8d079e45c
yalh76 <yalh@yahoo.com>
2022-04-24 03:44:07 +0200

    First implementation

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,13 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="deb1 deb2 php$YNH_DEFAULT_PHP_VERSION-deb1 php$YNH_DEFAULT_PHP_VERSION-deb2"
+pkg_dependencies="ntp ntpdate tzdata curl git imagemagick supervisor tzdata libc-dev zlib1g-dev xz-utils postgresql postgresql-common postgresql-client libidn11-dev redis-server"
+
+build_pkg_dependencies="build-essential patch libpq-dev"
+
+ruby_version=2.6
+
+nodejs_version=12
 
 #=================================================
 # PERSONAL HELPERS
