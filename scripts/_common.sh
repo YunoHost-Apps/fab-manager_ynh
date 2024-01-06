329bda2e278bda5aa1cf4a11f9faf002e6767a48
Alexandre Aubin <4533074+alexAubin@users.noreply.github.com>
2024-01-06 18:39:07 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,57 +1,6 @@
 #!/bin/bash
 
-#=================================================
-# COMMON VARIABLES
-#=================================================
-
-ruby_version="2.7.0"
-
 nodejs_version="18"
-
-# Workaround for Mastodon on Bullseye
-# See https://github.com/mastodon/mastodon/issues/15751#issuecomment-873594463
-if [ "$(lsb_release --codename --short)" = "bullseye" ]; then
-    case $YNH_ARCH in
-        amd64)
-            arch="x86_64"
-            ;;
-        arm64)
-            arch="aarch64"
-            ;;
-        armel|armhf)
-            arch="arm"
-            ;;
-        i386)
-            arch="i386"
-            ;;
-    esac
-    ld_preload="LD_PRELOAD=/usr/lib/$arch-linux-gnu/libjemalloc.so"
-else
-    ld_preload=""
-fi
-
-#=================================================
-# PERSONAL HELPERS
-#=================================================
-
-#=================================================
-# EXPERIMENTAL HELPERS
-#=================================================
-
-#=================================================
-# FUTURE OFFICIAL HELPERS
-#=================================================
-
-
-#!/bin/bash
-
-#=================================================
-# COMMON VARIABLES
-#=================================================
-
-# dependencies used by the app
-pkg_dependencies="imagemagick ffmpeg libpq-dev libxml2-dev libxslt1-dev file git-core g++ libprotobuf-dev protobuf-compiler pkg-config gcc autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3|libgdbm6 libgdbm-dev redis-server redis-tools postgresql postgresql-contrib libidn11-dev libicu-dev libjemalloc-dev curl apt-transport-https"
-
 RUBY_VERSION="3.2.2"
 
 # Workaround for Mastodon on Bullseye
