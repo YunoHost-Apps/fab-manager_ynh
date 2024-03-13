0d6a2ab4ad833c601140125a49340dc82a8d1146
Félix Piédallu <felix@piedallu.me>
2024-03-13 21:27:43 +0100

    Version 2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,11 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app (must be on a single line)
-pkg_dependencies="ntp ntpdate tzdata curl git imagemagick tzdata libc-dev zlib1g-dev xz-utils postgresql postgresql-common postgresql-client libidn11-dev redis-server"
-
-build_pkg_dependencies="build-essential patch libpq-dev"
-
 ruby_version="2.6.10"
 bundler_version=2.1.4
 
@@ -18,18 +13,10 @@ nodejs_version="14"
 # See https://github.com/mastodon/mastodon/issues/15751#issuecomment-873594463
 if [ "$(lsb_release --codename --short)" = "bullseye" ]; then
     case $YNH_ARCH in
-        amd64)
-            arch="x86_64"
-            ;;
-        arm64)
-            arch="aarch64"
-            ;;
-        armel|armhf)
-            arch="arm"
-            ;;
-        i386)
-            arch="i386"
-            ;;
+        amd64) arch="x86_64";;
+        arm64) arch="aarch64";;
+        armel|armhf) arch="arm";;
+        i386) arch="i386";;
     esac
     ld_preload="LD_PRELOAD=/usr/lib/$arch-linux-gnu/libjemalloc.so"
 else
