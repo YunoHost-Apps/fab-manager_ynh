b335cd54d211e09de5fdc30ec18fa566ca8f55bd
Félix Piédallu <felix@piedallu.me>
2024-06-28 11:15:23 +0200

    bundler needs to be installed as root

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -28,7 +28,7 @@ fi
 fabmanager_build_ruby() {
     pushd "$install_dir"
         ynh_use_ruby
-        ynh_exec_warn_less ynh_exec_as "$app" $ynh_gem install bundler
+        ynh_exec_warn_less $ynh_gem install bundler
         ynh_exec_warn_less ynh_exec_as "$app" bin/bundle config --global frozen 1
         ynh_exec_warn_less ynh_exec_as "$app" bin/bundle install --binstubs --without 'development test doc'
     popd
