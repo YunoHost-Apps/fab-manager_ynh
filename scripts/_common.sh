018ee4ef5357471e7fff6dfc09bde68eb0094990
eric_G <46165813+ericgaspar@users.noreply.github.com>
2025-09-25 22:33:55 +0200

    2.1

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #=================================================
-# COMMON VARIABLES
+# COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
 # Workaround for Mastodon on Bullseye
@@ -17,10 +17,6 @@ if [ "$(lsb_release --codename --short)" = "bullseye" ]; then
 else
     ld_preload=""
 fi
-
-#=================================================
-# PERSONAL HELPERS
-#=================================================
 
 check_password_policy() {
     password="$1"
@@ -45,13 +41,13 @@ check_password_policy() {
 }
 
 env_ruby() {
-    ynh_exec_as "$app" "$#REMOVEME? ynh_ruby_load_path" "$@"
+    ynh_exec_as_app "$#REMOVEME? ruby_load_path" "$@"
 }
 
 fabmanager_build_ruby() {
     pushd "$install_dir"
-        $ynh_gem update --system --no-document
-        $ynh_gem install bundler rake --no-document
+        gem update --system --no-document
+        gem install bundler rake --no-document
 
         env_ruby bin/bundle config --global frozen 1
         env_ruby bin/bundle config set without 'development test doc'
@@ -63,20 +59,21 @@ fabmanager_build_ruby() {
 
 fabmanager_build_ui() {
     pushd "$install_dir"
-        #REMOVEME? ynh_use_nodejs
-        ynh_exec_warn_less ynh_exec_as "$app" env "$ynh_node_load_PATH" yarn install
+        #REMOVEME?
+
+        ynh_hide_warnings ynh_exec_as_app node_load_PATH" yarn install
         env_ruby bash -c "set -a; source '$install_dir/.env'; set +a ; RAILS_ENV=production bin/bundle exec rake assets:precompile"
-        ynh_exec_warn_less ynh_exec_as "$app" env "$ynh_node_load_PATH" yarn cache clean --all
+        ynh_hide_warnings ynh_exec_as_app node_load_PATH" yarn cache clean --all
     popd
 }
 
 fabmanager_seed_db() {
     pushd "$install_dir"
-        ynh_replace_string --match_string="DateTime.current" --replace_string="DateTime.current - 1.days" --target_file="$install_dir/db/seeds.rb"
+        ynh_replace --match="DateTime.current" --replace="DateTime.current - 1.days" --file="$install_dir/db/seeds.rb"
         # Need superuser for the extensions configuration…
-        ynh_psql_execute_as_root --database="$db_name" --sql="ALTER USER $db_user WITH SUPERUSER;"
+        ynh_psql_db_shell  <<< "ALTER USER $db_user WITH SUPERUSER;"
         env_ruby bash -c "set -a; source '$install_dir/.env'; set +a ; RAILS_ENV=production ADMIN_EMAIL='$admin_mail' ADMIN_PASSWORD='$password' bin/bundle exec rails db:schema:load"
-        ynh_psql_execute_as_root --database="$db_name" --sql="ALTER USER $db_user WITH NOSUPERUSER;"
+        ynh_psql_db_shell  <<< "ALTER USER $db_user WITH NOSUPERUSER;"
         env_ruby bash -c "set -a; source '$install_dir/.env'; set +a ; RAILS_ENV=production ADMIN_EMAIL='$admin_mail' ADMIN_PASSWORD='$password' bin/bundle exec rails db:seed"
 
     popd
@@ -84,12 +81,12 @@ fabmanager_seed_db() {
 
 fabmanager_migrate_db() {
     pushd "$install_dir"
-        ynh_psql_execute_as_root --database="$db_name" --sql="ALTER USER $db_user WITH SUPERUSER;"
+        ynh_psql_db_shell  <<< "ALTER USER $db_user WITH SUPERUSER;"
         env_ruby bash -c "set -a; source '$install_dir/.env'; set +a ; RAILS_ENV=production bin/bundle exec rails db:migrate"
-        ynh_psql_execute_as_root --database="$db_name" --sql="ALTER USER $db_user WITH NOSUPERUSER;"
+        ynh_psql_db_shell  <<< "ALTER USER $db_user WITH NOSUPERUSER;"
     popd
 }
 
 fabmanager_configure_email() {
-    ynh_psql_execute_as_root --database="$db_name" --sql="INSERT INTO history_values (setting_id,value, created_at,updated_at) VALUES ((select id from settings where name='email_from'), '${mail_user}@${mail_domain}', NOW(),NOW());"
+    ynh_psql_db_shell  <<< "INSERT INTO history_values (setting_id,value, created_at,updated_at) VALUES ((select id from settings where name='email_from'), '${mail_user}@${mail_domain}', NOW(),NOW());"
 }
